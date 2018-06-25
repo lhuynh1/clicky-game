4f58cb2540598a6aae74f83c11be013209ea34d8
Lisa Huynh <lisaahuynh@yahoo.com>
2018-06-24 22:56:37 -0400

    functionality of app built in game section

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
-import React, { Component } from 'react';
-import DogCard from "./components/DogCard";
-import dog from "./dog.json";
-import './App.css';
+import React from 'react';
+import Jumbotron from "./components/Jumbotron"
+import GameSection from "./components/GameSection";
 
-class App extends Component {
-  state = {
-    dog,
-    clickedDog: [],
-    score: 0
-  };
 
-  clicked
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          
-          <h1 className="App-title">Welcome to the memory game.</h1>
-          <p>Featuring Dogs in Food! Check out the awesome instagram page here: 
-              <a href="https://www.instagram.com/dogs_infood/?hl=en"> @dogs_infood</a>
-          </p>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="container-fluid">
+    <Jumbotron />
+    <GameSection />
+  
+  </div>
+);
 
 export default App;
